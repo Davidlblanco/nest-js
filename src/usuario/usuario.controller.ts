859c2cffc65ce98a25e15f595bd5edcba17c1b00
David Blanco <david.blanco@maeztra.com>
2022-09-12 15:40:52 +0200

    usuario module

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -3,7 +3,11 @@ import { UsuarioService } from "./usuario.service"
 
 @Controller('users')
 export class UsuarioController {
-    private usuarioService = new UsuarioService()
+
+    constructor(
+        private usuarioService: UsuarioService
+    ) { }
+
     @Post()
     public cria(@Body() usuario) {
         const usuarioCriado = this.usuarioService.cria(usuario)
